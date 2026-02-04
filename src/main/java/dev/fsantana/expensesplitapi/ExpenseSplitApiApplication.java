7989eb681d9bfb6ca8fa903f18b7df7f53fe50bd
Felipe Souza <felipedb91@gmail.com>
2026-02-03 21:24:07 -0300

    chore: initial commit

---
+++ src/main/java/dev/fsantana/expensesplitapi/ExpenseSplitApiApplication.java
@@ -0,0 +1,13 @@
+package dev.fsantana.expensesplitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSplitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSplitApiApplication.class, args);
+	}
+
+}
