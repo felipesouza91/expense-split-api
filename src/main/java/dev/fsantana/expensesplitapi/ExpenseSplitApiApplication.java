83f470ef48e098ef148cdea2b60292f38c7919ef
Felipe S Santana <felipedb91@gmail.com>
2026-03-10 20:42:23 -0300

    feat: add UTC timezone on api

--- src/main/java/dev/fsantana/expensesplitapi/ExpenseSplitApiApplication.java
+++ src/main/java/dev/fsantana/expensesplitapi/ExpenseSplitApiApplication.java
@@ -3,11 +3,15 @@ package dev.fsantana.expensesplitapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ExpenseSplitApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExpenseSplitApiApplication.class, args);
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		SpringApplication application = new SpringApplication(ExpenseSplitApiApplication.class);
+		application.run(args);
 	}
 
 }
