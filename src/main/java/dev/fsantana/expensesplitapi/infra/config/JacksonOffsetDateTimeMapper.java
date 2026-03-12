bbd22c99cd999218038a88ba2511bb1ae28f4566
Felipe S Santana <felipedb91@gmail.com>
2026-03-12 20:31:59 -0300

    feat: date format

--- src/main/java/dev/fsantana/expensesplitapi/infra/config/JacksonOffsetDateTimeMapper.java
+++ src/main/java/dev/fsantana/expensesplitapi/infra/config/JacksonOffsetDateTimeMapper.java
@@ -1,18 +1,54 @@
 package dev.fsantana.expensesplitapi.infra.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonOffsetDateTimeMapper{
 
+    @Primary
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
-    }
+        JavaTimeModule module = new JavaTimeModule();
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+
+        module.addSerializer(OffsetDateTime.class,
+                new JsonSerializer<>() {
+                    @Override
+                    public void serialize(OffsetDateTime value, JsonGenerator gen,
+                                          SerializerProvider provider) throws IOException {
+                        gen.writeString(value.format(formatter));
+                    }
+                });
+
+        module.addDeserializer(OffsetDateTime.class,
+                new JsonDeserializer<>() {
+                    @Override
+                    public OffsetDateTime deserialize(JsonParser p,
+                                                      DeserializationContext ctx) throws IOException {
+                        return OffsetDateTime.parse(p.getText(), formatter);
+                    }
+                });
+
+        return JsonMapper.builder()
+                .addModule(module)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
+    }
 }
