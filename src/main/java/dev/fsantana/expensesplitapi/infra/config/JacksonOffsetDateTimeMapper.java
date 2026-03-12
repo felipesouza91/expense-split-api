095b2917bc9f7e0616683ca3db77d08ae5bc3d61
Felipe S Santana <felipedb91@gmail.com>
2026-03-11 21:55:24 -0300

    chore: add date format

---
+++ src/main/java/dev/fsantana/expensesplitapi/infra/config/JacksonOffsetDateTimeMapper.java
@@ -0,0 +1,18 @@
+package dev.fsantana.expensesplitapi.infra.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonOffsetDateTimeMapper{
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+}
